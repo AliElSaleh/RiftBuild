c7cbe89346df2eeae1e058d50e10ad322ede34db
Ali El Saleh <ali@Alis-Laptop.local>
2024-06-11 22:04:48 +1000

    fix script on mac. do colored log

--- Scripts/build_unix.sh
+++ Scripts/build_unix.sh
@@ -6,9 +6,20 @@ mkdir -p Build
 mkdir -p Build/Dist
 mkdir -p Intermediate
 
+platform='unknown'
+unamestr=$(uname)
+if [ "$unamestr" = 'Linux' ]; then
+   platform='Linux'
+   LinuxLinkerFlags="-LSource/Libraries/Linux -lUUIDS"
+elif [ "$unamestr" = 'Darwin' ]; then
+   platform='Mac'
+elif [ "$unamestr" = 'FreeBSD' ]; then
+   platform='BSD'
+fi
+
 CompilerFlags="-std=c17 -Os -fdeclspec -fno-exceptions -fno-math-errno -fdiagnostics-absolute-paths -fno-delete-null-pointer-checks -fno-strict-overflow -fno-strict-aliasing -fstack-protector-strong -Wall -Wextra -Wshadow -Wconversion -Wpedantic -Winfinite-recursion -Wmissing-prototypes -Warray-bounds -Wmisleading-indentation -Wunused -Wuninitialized -Wno-empty-translation-unit -Wno-gnu-zero-variadic-macro-arguments -Wno-unused-parameter -Wunused-function -Werror=vla -Werror=alloca -Werror=implicit-function-declaration -Werror=pointer-arith -Werror=shadow -Werror=uninitialized -Werror=array-bounds -Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion -Werror=float-conversion -Werror=shorten-64-to-32 -mno-stack-arg-probe -mstack-probe-size=999999999 -ferror-limit=1 -fPIE -DNO_ASSERT -DNO_PROFILING -D_NO_CRT_STDIO_INLINE -DRIFT_STATIC -DNO_LOG_FILE -DRIFTBUILD_VERSION_STRING=\"0.9.5\" -DRIFTBUILD_MAJOR_VERSION=0 -DRIFTBUILD_MINOR_VERSION=9 -DRIFTBUILD_PATCH_VERSION=5 -ISource/Core -ISource/Libraries/Vendor -ISource/Libraries/Linux"
 
-echo "Compiling sources (Linux)"
+echo "Compiling sources (${platform})"
 
 clang -c "Source/Core/Clock/Clock.c"             $CompilerFlags -o "Intermediate/Clock.c.o"
 clang -c "Source/Core/Math/Math.c"               $CompilerFlags -o "Intermediate/Math.c.o"
@@ -25,19 +36,9 @@ clang -c "Source/MSVCBackend.c"                  $CompilerFlags -o "Intermediate
 clang -c "Source/Parse.c"                        $CompilerFlags -o "Intermediate/Parse.c.o"
 clang -c "Source/Exporter.c"                     $CompilerFlags -o "Intermediate/Exporter.c.o"
 
-platform='unknown'
-unamestr=$(uname)
-if [ "$unamestr" = 'Linux' ]; then
-   platform='Linux'
-elif [ "$unamestr" = 'Darwin' ]; then
-   platform='Mac'
-elif [ "$unamestr" = 'FreeBSD' ]; then
-   platform='BSD'
-fi
-
-clang -c "Source/Core/Platform/Platform_$(platform).c" $CompilerFlags -o "Intermediate/Platform_$(platform).c.o"
+clang -c "Source/Core/Platform/Platform_${platform}.c" $CompilerFlags -o "Intermediate/Platform_${platform}.c.o"
 
 echo "Building binary"
-clang -o Build/Dist/riftbuild "Intermediate/Clock.c.o" "Intermediate/Math.c.o" "Intermediate/Memory.c.o" "Intermediate/Allocators.c.o" "Intermediate/StringUtils.c.o" "Intermediate/Containers.c.o" "Intermediate/Log.c.o" "Intermediate/Globals.c.o" "Intermediate/EngineUtils.c.o" "Intermediate/Platform_$(platform).c.o" "Intermediate/Program.c.o" "Intermediate/CBackend.c.o" "Intermediate/MSVCBackend.c.o" "Intermediate/Parse.c.o" "Intermediate/Exporter.c.o" -LSource/Libraries/Linux -lUUIDS -Wl,-rpath,'$ORIGIN'
+clang -o Build/Dist/riftbuild "Intermediate/Clock.c.o" "Intermediate/Math.c.o" "Intermediate/Memory.c.o" "Intermediate/Allocators.c.o" "Intermediate/StringUtils.c.o" "Intermediate/Containers.c.o" "Intermediate/Log.c.o" "Intermediate/Globals.c.o" "Intermediate/EngineUtils.c.o" "Intermediate/Platform_${platform}.c.o" "Intermediate/Program.c.o" "Intermediate/CBackend.c.o" "Intermediate/MSVCBackend.c.o" "Intermediate/Parse.c.o" "Intermediate/Exporter.c.o" ${LinuxLinkerFlags} -Wl,-rpath,'$ORIGIN'
 
-echo "  Done: Build/Dist/riftbuild"
+echo "\033[0;32m  Done: Build/Dist/riftbuild\033[0m"
