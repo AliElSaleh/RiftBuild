ab9a39754f73e200a047d3922181dc4ea7ce47b1
Ali <elsaleh78@gmail.com>
2025-08-21 12:15:20 +1000

    update build files

--- build.bat
+++ build.bat
@@ -6,20 +6,21 @@ where /q clang || (
 )
 
 set ScriptPath=%~dp0
-set CompilerFlags= -std=c99 -Os -mstack-probe-size=999999999 -fno-builtin-memcpy -fno-omit-frame-pointer -fno-exceptions -fno-math-errno -fno-strict-overflow -fno-strict-aliasing -Wall -Wextra -Wshadow -Wconversion -Wmissing-prototypes -Warray-bounds -Wunused -Wuninitialized -Wno-empty-translation-unit -Wno-gnu-zero-variadic-macro-arguments -Wno-unused-parameter -Wno-missing-field-initializers -Wno-missing-braces -Wno-incompatible-library-redeclaration -Wno-typedef-redefinition -Wunused-function -Werror=vla -Werror=implicit-function-declaration -Werror=pointer-arith -Werror=shadow -Werror=uninitialized -Werror=array-bounds -Werror=implicit -Werror=incompatible-pointer-types -Werror=int-conversion -DNO_ASSERT -DNO_PROFILING -D_NO_CRT_STDIO_INLINE -DRIFT_STATIC -DWIN32_LEAN_AND_MEAN -DRIFTBUILD_VERSION_STRING=\"0.9.9-alpha\" -DRIFTBUILD_MAJOR_VERSION=0 -DRIFTBUILD_MINOR_VERSION=9 -DRIFTBUILD_PATCH_VERSION=9 -ISource
-:: set LinkerFlags=
-:: set Defines=
-:: set Libraries=
-:: set LibraryDirectories=
+set CompilerFlags=-std=c99 -fno-builtin-memcpy -fno-omit-frame-pointer -fno-exceptions -fno-math-errno -funroll-loops -fno-rtti -fno-strict-overflow -fno-strict-aliasing -Wall -Wextra -Wshadow -Wconversion -Wmissing-prototypes -Wunused -Wuninitialized -Werror -Wpedantic -Wno-typedef-redefinition -Wno-unused-parameter -Wno-gnu-zero-variadic-macro-arguments -Wno-unused-command-line-argument -nostdlib -fno-builtin -ffreestanding -msse2 -mstack-probe-size=999999999 -Os -finline-functions -finline-hint-functions
+set IncludeFlags=-ISource
+set LinkerFlags=-nostdlib -Wl,-entry:EntryPoint,-subsystem:console -Xlinker /stack:0x800000,0x800000
+set Defines=-DRIFT_STATIC -DNO_ASSERT -D_NO_CRT_STDIO_INLINE -DRIFTBUILD_VERSION_STRING=\"0.1.0-beta\" -DRIFTBUILD_MAJOR_VERSION=0 -DRIFTBUILD_MINOR_VERSION=1 -DRIFTBUILD_PATCH_VERSION=0
+set Libraries=-lkernel32 -luser32 -lshell32 -lole32 -ladvapi32 -lntdll -lshlwapi -lbcrypt -loleaut32 -lmicrosoft_craziness
+set LibraryPaths=-LSource\Libraries\Windows
 
 echo Compiling sources (Windows)
 
-clang "Source/Core/Memory.c" "Source/Core/StringUtils.c" "Source/Core/Log.c" "Source/Core/Platform_Core.c" "Source/Core/Platform_Windows.c" "Source/Program.c" "Source/Backend.c" "Source/Parse.c" "Source/Exporter.c" %CompilerFlags% -o RiftBuild.exe -nostdlib -Wl,-entry:EntryPoint,-subsystem:console -Xlinker /stack:0x800000,0x800000 -LSource\Libraries\Windows -lkernel32 -luser32 -lopengl32 -lshell32 -lgdi32 -lcomdlg32 -lcomctl32 -lws2_32 -lwinmm -lnetapi32 -lole32 -ladvapi32 -lwldap32 -lcrypt32 -lntdll -lrpcrt4 -lshlwapi -ldbghelp -lbcrypt -lversion -limm32 -lcfgmgr32 -lsetupapi -loleaut32 -luuid -lodbc32 -lodbccp32 -ldelayimp -lmicrosoft_craziness || goto end
+clang "Source/Core/Memory.c" "Source/Core/StringUtils.c" "Source/Core/Log.c" "Source/Core/Platform_Core.c" "Source/Core/Platform_Windows.c" "Source/Program.c" "Source/Backend.c" "Source/Parse.c" "Source/Exporter.c" %CompilerFlags% %Defines% %IncludeFlags% -o RiftBuild.exe %LinkerFlags% %LibraryPaths% %Libraries% || goto end
 
 echo [32m  Done: %ScriptPath%RiftBuild.exe[0m
 
 :end
-:: pause if double clicked
+:: pause if we double clicked this in a file explorer
 setlocal enabledelayedexpansion
 set testl=%cmdcmdline:"=%
 set testr=!testl:%~nx0=!
